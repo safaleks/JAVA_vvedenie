6a26c0e464e4b9484d7429db1180a9ef8ecc4fcd
safaleks <safaleks@mail.ru>
2021-09-09 14:24:49 +0300

    Задание 1 (ДЗ-1)

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 150;
-        int count = 5;
-        int total = price * count;
+        int price = 2000000000;
+        int count = 500000000;
+        int total = price + count;
         System.out.println(total);
     }
 }
